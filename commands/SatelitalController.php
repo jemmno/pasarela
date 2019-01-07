96eeefe5e1c62f270331c820514c00fd31244617
José Emmanuel Noguera <nogueraruiz@gmail.com>
2019-01-07 19:25:14 -0300

    WIP get first grupo de posiciones

--- commands/SatelitalController.php
+++ commands/SatelitalController.php
@@ -16,12 +16,16 @@ class SatelitalController extends Controller
     public $client= '';
     public $access= '';
     public $password= '';
+    public $credenciales = [];
     
     public function init()
     {
         $this->access = Config::env('ACCESS_ID');
         $this->password = Config::env('PASSWORD');
+        $this->credenciales = ['access_id' => $this->access, 'password' => $this->password];
+
         \Yii::info('Preguntando desde: ' . date('l jS \of F Y h:i:s A') . "\n", 'poller');
+        
         if (!$this->access || !$this->password) {
             die('Debe setear access_id y password de orbcomm.');
         }
@@ -32,13 +36,28 @@ class SatelitalController extends Controller
 
     public function actionPreguntar()
     {
-        return self::getHoraServer();
+        $horaUTC = self::getHoraServer();
+        print json_encode(self::getFirstLocationGroup($horaUTC));
     }
 
     public function getHoraServer()
     {
-        $getHoraResponse = $this->client->get('info_utc_time.json', ['access_id' => $this->access, 'password' => $this->password])->send();
-        echo $getHoraResponse;
+        $getHoraResponse = $this->client->get('info_utc_time.json', $this->credenciales)->send();
+        if ($getHoraResponse->isOk) {
+            return $hora = $getHoraResponse->data;
+        }
+        
+    }
+
+    public function getFirstLocationGroup(string $hora)
+    {
+        $params = [];
+        $params = $this->credenciales;
+        $params['start_utc'] = $hora;
+        $getFirstResponse = $this->client->get('get_return_messages.json', $params)->send();
+        if ($getFirstResponse->isOk) {
+            return $getFirstResponse->data;
+        }
     }
 
 }
