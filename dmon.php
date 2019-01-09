6b440f26884a1165e6ea8e3d5e90e27b8b34bedb
Emmanuel Noguera <nogueraruiz@gmail.com>
2019-01-09 12:50:05 -0300

    ruta estatica yii en dmon

--- dmon.php
+++ dmon.php
@@ -1,6 +1,6 @@
 <?php
 
-$output = shell_exec('ps -c php -f');
+$output = shell_exec('ps -C php -f');
 if (strpos($output, "php yii pasarela/escuchar")===false) { 
-  shell_exec('php yii pasarela/escuchar  > running.log 2>&1 &');
+  shell_exec('nohup php /var/www/pasarela/yii pasarela/escuchar  > running.log 2>&1 &');
 }
